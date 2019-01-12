937a6ea2262dc8c32a7513453a38fd69a4621738
2018louysa <soudini@SurfaceSoudini.localdomain>
2019-01-12 23:35:07 +0000

    fixed auth for real this time (I hope)

--- backend/server.js
+++ backend/server.js
@@ -48,7 +48,7 @@ router.post("/getUserInfo", (req, res) => {
     const requestBody = {
       grant_type : "authorization_code",
       code : code,
-      redirect_uri : "http://138.195.139.246/",
+      redirect_uri : "http://138.195.139.246/oauthend",
       client_id : "279c525e5961df88feb2b6053f210f7537265270",
       client_secret : "f9e8e9c0a1a1eb060601e491286613f33f76ae73"
     }
