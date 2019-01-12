645e8d42c693d1a3981f13ebb87c0c5d15c49d4e
2018louysa <soudini@SurfaceSoudini.localdomain>
2019-01-12 23:03:06 +0000

    test 4

--- client/src/components/oauthend.js
+++ client/src/components/oauthend.js
@@ -31,7 +31,7 @@ class OauthEnd extends Component {
       dict[string[i][0]] = string[i][1];
     }
     cookies.set('code', dict["code"]);
-    console.log(cookies.get("code"));
+    console.log(cookies.get("code"),dict["code"],dict);
     this.props.history.push("/");
     this.searchDataFromDb();
     console.log(cookies)
