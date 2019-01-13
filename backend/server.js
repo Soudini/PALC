21b6c16710ae9e5f1d08a43cb2d42e2d2722e3ab
2018louysa <soudini@SurfaceSoudini.localdomain>
2019-01-13 00:12:57 +0000

    auth strat fixed ?

--- backend/server.js
+++ backend/server.js
@@ -69,7 +69,7 @@ router.post("/getUserInfo", (req, res) => {
                     else {
                       axios.get("https://auth.viarezo.fr/api/user/show/me", {headers : {Authorization: 'Bearer '.concat(data.access_token)}})
                       .then(response => {console.log(response.data);
-                                        return res.json({data: response.data})})}
+                                        return res.json({data: JSON.parse(response).data})})}
 
                   });
 
