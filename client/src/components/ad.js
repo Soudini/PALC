fa9cf00640772e12e13fd0cf2e562ad6e7c3a2c9
2018louysa <soudini@SurfaceSoudini.localdomain>
2019-01-15 10:28:50 +0000

    go to home only if delete confirm

--- client/src/components/ad.js
+++ client/src/components/ad.js
@@ -22,8 +22,9 @@ export class Page extends Component {
   handleDelete =()=> {
 
       if (window.confirm("Voulez vous vraiment supprimer cette annonce ?"))
-      {axios.post("/api/deleteData", {id :this.props.match.params.id });}
-      this.props.history.push("/");
+      {axios.post("/api/deleteData", {id :this.props.match.params.id });
+    this.props.history.push("/");}
+
 
 
   }
