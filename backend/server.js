491f02b82ee7454ec2f131c931a8f375e38bf501
2018louysa <soudini@SurfaceSoudini.localdomain>
2019-01-27 14:48:20 +0000

    log show_button

--- backend/server.js
+++ backend/server.js
@@ -91,7 +91,8 @@ router.post("/searchById", (req, res) => {
   auth_author_login = crypto.AES.decrypt(auth, keyEncrypt).toString(crypto.enc.Utf8);
   Data.findById(id).exec((err, data) => {
     if (err) return res.json({ success: false, error: err });
-    data["show_button"] = (auth_author_login == data.author_login | admin.includes(auth_author_login));
+    data.show_button = (auth_author_login == data.author_login | admin.includes(auth_author_login));
+    console.log(data.show_button);
     return res.json({ success: true, data: data });
   });
 });
