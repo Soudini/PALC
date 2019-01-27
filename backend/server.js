535e1bf05d8f759834d2b3b70da1612e858bbef5
2018louysa <soudini@SurfaceSoudini.localdomain>
2019-01-27 14:21:46 +0000

    added admin file at root

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+import admin from "../admin.js";
+
 const mongoose = require("mongoose");
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -11,6 +13,7 @@ const router = express.Router();
 const request = require("request");
 const crypto = require("crypto-js");
 
+
 mongoose.set('useFindAndModify', false);
 
 app.use(bodyParser({limit: '50mb'}));
@@ -18,7 +21,7 @@ app.use(bodyParser({limit: '50mb'}));
 const dbRoute = "mongodb://Server:dTvTZv4m75ucB5E@ds145193.mlab.com:45193/objets-trouves";
 keyEncrypt = "1sd'o-tevtb!"
 
-admin = ["2018louysa"]
+console.log(admin);
 
 // connects our back end code with the database
 mongoose.connect(
