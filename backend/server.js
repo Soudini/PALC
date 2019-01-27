2af73cfd4ec6ffa246c836593b39dbb046f9b6ff
2018louysa <soudini@SurfaceSoudini.localdomain>
2019-01-27 14:59:46 +0000

    fix

--- backend/server.js
+++ backend/server.js
@@ -92,7 +92,7 @@ router.post("/searchById", (req, res) => {
   Data.findById(id).exec((err, data) => {
     if (err) return res.json({ success: false, error: err });
     data.show_button = (auth_author_login == data.author_login | admin.includes(auth_author_login));
-    console.log(data.keys);
+    console.log(Object.keys(data));
     return res.json({ success: true, data: data });
   });
 });
