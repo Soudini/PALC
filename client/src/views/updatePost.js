23166a26590b2cd7c65977af9e31134f85de35f4
2018louysa <soudini@SurfaceSoudini.localdomain>
2019-01-28 21:04:35 +0100

    added auth log

--- client/src/views/updatePost.js
+++ client/src/views/updatePost.js
@@ -270,6 +270,7 @@ class CreatePost extends Component {
   }
 
   searchDataFromDb = () => {
+    console.log(cookies.get("auth"));
     axios.post("/api/searchById", {id :this.props.match.params.id, auth : cookies.get("auth")})
       .then(data => data.data).then(res => {for (let i in res.data) {this.setState({[i] : res.data[i]})}});
   };
