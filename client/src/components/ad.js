69d46eb382102fd5dc0cebee45a779f44bfb7af8
2018louysa <soudini@SurfaceSoudini.localdomain>
2019-02-06 19:44:27 +0100

    test ad margin bottom

--- client/src/components/ad.js
+++ client/src/components/ad.js
@@ -129,7 +129,7 @@ export default class Ad extends Component {
 
   render() {
     let className = "card h-100"//+ (this.props.data.type === "search" ? " bg-secondary" : " bg-success");
-    return (<a className="style-1" href="#" style={{"marginTop":"1rem" , "marginLeft" : "auto", "marginRight" : "auto"}}>
+    return (<a className="style-1" href="#" style={{"marginTop":"1rem" , "marginLeft" : "auto", "marginRight" : "auto", "marginBottom": "1rem"}}>
               <div className={className} style={{"width": "18rem"}} >
                 <LinkContainer to={"/ad/"+this.props.data._id}>
                   <img className="card-img-top test" style={{"maxHeight" : "1px"}} src={this.props.data.thumbnail} />
