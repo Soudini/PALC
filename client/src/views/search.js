84bc1a3b77b4a8063d0fa88c7e3ad0348ecd9984
2018louysa <soudini@SurfaceSoudini.localdomain>
2019-02-07 21:40:30 +0100

    update search

--- client/src/views/search.js
+++ client/src/views/search.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import axios from "axios";
 import Ad from "../components/ad.js";
 
-export default class Search extends Component {
+export default class Home extends Component {
   state = {
     data: [],
+    number : 16,
+    page:0
   }
 
   componentDidMount() {
@@ -12,7 +14,9 @@ export default class Search extends Component {
     if (!this.state.intervalIsSet) {
       let interval = setInterval(this.getDataFromDb, 1000);
       this.setState({ intervalIsSet: interval });
+
     }
+    this.getNumberAds();
   }
 
   // never let a process live forever
@@ -24,40 +28,49 @@ export default class Search extends Component {
     }
   }
 
+  getNumberAds = () => {
+    axios.post("/api/getNumberAds",{search : {type: "search"}})
+      .then(data => data.data).then(res => {this.setState({ pageNumber: res.data }, console.log("number of ads",res.data))});
+
+  }
   searchDataFromDb = () => {
-    axios.post("/api/searchData", {search : {"type": "search"}})
+    console.log("searchDataFromDb",this.state);
+    axios.post("/api/searchData", {search : {type: "search"}, number : this.state.number , page:this.state.page})
       .then(data => data.data).then(res => {this.setState({ data: res.data })});
 
   };
 
+  changePage = (i) => {
+    this.state.page = i ;
+    this.searchDataFromDb();
+    console.log("this.state.page", this.state.page);
+  }
 
+  previousNext = (i) => {
+    this.state.page = this.state.page + i ;
+    this.searchDataFromDb();
+  }
 
   render () {
 
+      let pagination = [];
+      for (let i = 0; i<this.state.pageNumber / this.state.number; i++){
+        pagination.push(i)
+      }
+      console.log("render",this.state);
       return (
-        <div className="row justify-content-center">
-        <div className="card-deck">
-            {this.state.data.map(dat => (<Ad key={dat._id} data={dat}/>))}
-        </div>
-        </div>
+          <div className="row justify-content-center">
+            <div className="card-deck">
+                {this.state.data.map(dat => (<Ad key={dat._id} data={dat}/>))}
+            </div>
+            <nav aria-label="Page navigation example">
+              <ul class="pagination">
+                <li class="page-item"><a class="page-link" href="#" onClick={() => this.previousNext(-1)}>Previous</a></li>
+                {pagination.map( i => (<li class="page-item"><a class="page-link" href="#" onClick={() => this.changePage(i)}>{i+1}</a></li>))}
+                <li class="page-item"><a class="page-link" href="#" onClick={() => this.previousNext(1)}>Next</a></li>
+              </ul>
+            </nav>
+          </div>
       )
    }
 }
-
-
-/*          <div>
-            <ul>
-            {this.state.data.length <= 0
-              ? "NO DB ENTRIES YET"
-              : this.state.data.map(dat => (
-                  <li style={{ padding: "10px" }} key={dat.id}>
-                    <span style={{ color: "gray" }}> id: </span> {dat.id} <br />
-                    <span style={{ color: "gray" }}> data: </span>
-                    <ul>
-                      <li>{dat.type}</li>
-                      <li>{dat.description}</li>
-                    </ul>
-                  </li>
-                ))}
-            </ul>
-          </div>*/
