277115352cbc9370592791dd64d7f474c553cd67
2018louysa <soudini@SurfaceSoudini.localdomain>
2019-02-07 19:47:04 +0100

    added log

--- client/src/views/home.js
+++ client/src/views/home.js
@@ -34,6 +34,7 @@ export default class Home extends Component {
 
   }
   searchDataFromDb = () => {
+    console.log(this.state);
     axios.post("/api/searchData", {search : {}, number : this.state.number , page:this.state.page})
       .then(data => data.data).then(res => {this.setState({ data: res.data })});
 
@@ -55,7 +56,7 @@ export default class Home extends Component {
             <nav aria-label="Page navigation example">
               <ul class="pagination">
                 <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                {pagination.map( i => (<li class="page-item"><a class="page-link" href="" onClick={() => {this.setState({page : i}) ;  this.searchDataFromDb()}}>i</a></li>))}
+                {pagination.map( i => (<li class="page-item"><a class="page-link" href="" onClick={() => {this.setState({page : i}) ;  this.searchDataFromDb()}}>{i}</a></li>))}
                 <li class="page-item"><a class="page-link" href="#">Next</a></li>
               </ul>
             </nav>
