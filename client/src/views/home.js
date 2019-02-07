7c786d8037621b9f79f14271d802667d01fd6695
2018louysa <soudini@SurfaceSoudini.localdomain>
2019-02-07 20:52:15 +0100

    fix and number page

--- client/src/views/home.js
+++ client/src/views/home.js
@@ -60,7 +60,7 @@ export default class Home extends Component {
             <nav aria-label="Page navigation example">
               <ul class="pagination">
                 <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                {pagination.map( i => (<li class="page-item"><a class="page-link" href="" onClick={changePage(i)}>{i}</a></li>))}
+                {pagination.map( i => (<li class="page-item"><a class="page-link" href="" onClick={changePage(i)}>{i+1}</a></li>))}
                 <li class="page-item"><a class="page-link" href="#">Next</a></li>
               </ul>
             </nav>
