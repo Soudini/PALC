7c851bbbcd89cde476d29f0757538fc618a52bc0
2018louysa <soudini@SurfaceSoudini.localdomain>
2019-02-09 15:11:49 +0100

    added log

--- client/src/views/searchEngine.js
+++ client/src/views/searchEngine.js
@@ -58,6 +58,7 @@ export default class Perso extends Component {
       for (let i = 0; i<this.state.pageNumber / this.state.number; i++){
         pagination.push(i)
       }
+      console.log(pagination)
       return (
         <div className="row justify-content-center">
           <div className="card-deck">
