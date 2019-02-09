635b271e0523de5e14586a74f5e2d9fb2fd2c497
2018louysa <soudini@SurfaceSoudini.localdomain>
2019-02-09 14:48:32 +0100

    ban ok !

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const dbRoute = "mongodb://localhost/ads";
 let keyEncrypt = "1sd'o-tevtb!";
 let reCaptchaKey =  "6LcpTZAUAAAAAHx6KlhmNV7PjWFBENNzpOVjCe3V";
 let admin = ["2018louysa"];
-let ban = ["2018louysa"];
+let ban = [];
 
 
 // connects our back end code with the database
