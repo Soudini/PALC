d9049771b8780e99e70b6e4e128eeee4d6f58849
2018louysa <soudini@SurfaceSoudini.localdomain>
2019-02-09 15:14:40 +0100

    fix

--- client/src/views/searchEngine.js
+++ client/src/views/searchEngine.js
@@ -26,7 +26,7 @@ export default class Perso extends Component {
 
   }
   searchDataFromDb = (searchText) => {
-      axios.post("/api/searchData", {search : { "$or":[{"type":{"$regex": searchText, "$options" : "i"}},{"author":{"$regex": searchText, "$options" : "i"}},{"reward":{"$regex": searchText, "$options" : "i"}},{"description":{"$regex": searchText, "$options" : "i"}},{"title":{"$regex": searchText, "$options" : "i"}} ]}})
+      axios.post("/api/searchData", {search : { "$or":[{"type":{"$regex": searchText, "$options" : "i"}},{"author":{"$regex": searchText, "$options" : "i"}},{"reward":{"$regex": searchText, "$options" : "i"}},{"description":{"$regex": searchText, "$options" : "i"}},{"title":{"$regex": searchText, "$options" : "i"}} ]},number : this.state.number , page:this.state.page})
         .then(data => data.data).then(res => {this.setState({ data: res.data })});
   };
 
