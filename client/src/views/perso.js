e2d11f45fa92946874fe3ea563d5b627fc705d57
2018louysa <soudini@SurfaceSoudini.localdomain>
2019-02-26 18:10:17 +0100

    fix perso.js cookies

--- client/src/views/perso.js
+++ client/src/views/perso.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import axios from "axios";
 import Display from "../components/display.js";
+import Cookies from 'universal-cookie';
+
+const cookies = new Cookies();
 export default class Home extends Component {
 
   render () {
