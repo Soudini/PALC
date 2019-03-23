e92c41d39c03078e9dbd574add792f42d82547cb
Raiki Ali <ali.raiki@student-cs.fr>
2019-03-23 15:14:57 +0100

    Adding logo to welcome page

--- client/src/views/home.js
+++ client/src/views/home.js
@@ -9,6 +9,7 @@ export default class All extends Component {
   render() {
     return (
       <div className="container-fluid text-center">
+        <br></br>
         <h1>Bienvenue sur Palc ! </h1>
         <img src={logopalccouleur} class="img-fluid" alt="Responsive image" />
         <p> Mais qu'est ce que palc peut bien vouloir dire ?  Ca veut dire "Pinte à la clé"</p>
