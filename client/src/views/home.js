ab0872b1197b91022f5619f0a9327e59ccbde266
Raiki Ali <ali.raiki@student-cs.fr>
2019-03-23 14:58:05 +0100

    Adding logo to welcome page

--- client/src/views/home.js
+++ client/src/views/home.js
@@ -3,12 +3,13 @@ import axios from "axios";
 import Display from "../components/display.js";
 export default class All extends Component {
 
-  render () {
-      return (
-        <div className="container-fluid">
-          <h1>Bienvenue sur Palc ! </h1>
-          <p> Mais qu'est ce que palc peut bien vouloir dire ?  Ca veut dire "Pinte à la clé"</p>
-        </div>
-      )
-   }
+  render() {
+    return (
+      <div className="container-fluid">
+        <h1>Bienvenue sur Palc ! </h1>
+        <img href="%PUBLIC_URL%/logopalccouleur.png" class="img-fluid" alt="Responsive image" />
+        <p> Mais qu'est ce que palc peut bien vouloir dire ?  Ca veut dire "Pinte à la clé"</p>
+      </div>
+    )
+  }
 }
