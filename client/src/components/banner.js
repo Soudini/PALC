0aaca37bc7fdbfcd297d78060fc98a2b6a0f5abd
Raiki Ali <ali.raiki@student-cs.fr>
2019-03-23 20:43:30 +0100

    Testing colors

--- client/src/components/banner.js
+++ client/src/components/banner.js
@@ -72,7 +72,7 @@ class Banner extends Component {
 
   render() {
     return (
-      <nav className="navbar fixed-top navbar-expand-xl navbar-light col-lg-12">
+      <nav className="navbar fixed-top navbar-expand-xl navbar-dark col-lg-12">
         <a className="navbar-brand" href="" onClick={() => this.handlePageChange("")}>Palc</a>
         <button className="navbar-toggler" id="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
