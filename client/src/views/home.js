e7e1575c36a1509291e45e224e31e13a4f12b03d
Raiki Ali <ali.raiki@student-cs.fr>
2019-03-23 15:20:44 +0100

    Adding logo to welcome page

--- client/src/views/home.js
+++ client/src/views/home.js
@@ -8,12 +8,14 @@ export default class All extends Component {
 
   render() {
     return (
-      <div className="container-fluid text-center bg-primary">
-        <br></br>
-        <h1>Bienvenue sur Palc ! </h1>
-        <img src={logopalccouleur} class="img-fluid" alt="Responsive image" />
-        <p> Mais qu'est ce que palc peut bien vouloir dire ?  Ca veut dire "Pinte à la clé"</p>
-      </div>
+      <section className="bg-primary">
+        <div className="container-fluid text-center">
+          <br></br>
+          <h1>Bienvenue sur Palc ! </h1>
+          <img src={logopalccouleur} class="img-fluid" alt="Responsive image" />
+          <p> Mais qu'est ce que palc peut bien vouloir dire ?  Ca veut dire "Pinte à la clé"</p>
+        </div>
+      </section>
     )
   }
 }
