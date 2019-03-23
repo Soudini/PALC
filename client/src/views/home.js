e26eb8e0e90d4ae7313587078ac57f15348f4820
Raiki Ali <ali.raiki@student-cs.fr>
2019-03-23 20:55:04 +0100

    Change logo size in navbar

--- client/src/views/home.js
+++ client/src/views/home.js
@@ -16,7 +16,7 @@ export default class All extends Component {
               <br></br>
               <h1 class="section-heading"> Bienvenue sur PALC ! </h1>
               <hr class="light my-4" />
-              <img src={logopalccouleur} class="img-fluid" alt="Responsive image" />
+              <img src={logopalccouleur} id="image_welcome_page" class="img-fluid" alt="Responsive image" />
               <p> Mais qu'est ce que PALC peut bien vouloir dire ?  Ca veut dire "Pinte à la clé" ! Tu peux trouver ici les objets trouvés, ou faire une annonce si tu as trouvé un objet.</p>
             </div>
           </div>
