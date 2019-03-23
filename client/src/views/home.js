254befb9c9263b10d411405a145077533232e76c
Raiki Ali <ali.raiki@student-cs.fr>
2019-03-23 15:50:47 +0100

    Add padding to welcome page

--- client/src/views/home.js
+++ client/src/views/home.js
@@ -9,11 +9,17 @@ export default class All extends Component {
   render() {
     return (
       <section>
-        <div className="container-fondus text-center">
-          <br></br>
-          <h1>Bienvenue sur PALC ! </h1>
-          <img src={logopalccouleur} class="img-fluid" alt="Responsive image" />
-          <p> Mais qu'est ce que PALC peut bien vouloir dire ?  Ca veut dire "Pinte à la clé" ! Tu peux trouver ici les objets trouvés, ou faire une annonce si tu as trouvé un objet.</p>
+        <div className="container-fondus ">
+          <div className="row">
+            <div className="col-lg-8 mx-auto text-center">
+
+              <br></br>
+              <h1 class="section-heading"> Bienvenue sur PALC ! </h1>
+              <hr class="light my-4" />
+              <img src={logopalccouleur} class="img-fluid" alt="Responsive image" />
+              <p> Mais qu'est ce que PALC peut bien vouloir dire ?  Ca veut dire "Pinte à la clé" ! Tu peux trouver ici les objets trouvés, ou faire une annonce si tu as trouvé un objet.</p>
+            </div>
+          </div>
         </div>
       </section>
     )
