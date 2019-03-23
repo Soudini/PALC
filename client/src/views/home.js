4ac93f74af325f7a35eddcc730d12c18f16477aa
Raiki Ali <ali.raiki@student-cs.fr>
2019-03-23 15:05:31 +0100

    Adding logo to welcome page

--- client/src/views/home.js
+++ client/src/views/home.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import axios from "axios";
 import Display from "../components/display.js";
+import logopalccouleur from '../../public/logopalccouleur'
 export default class All extends Component {
 
   render() {
     return (
-      <div className="container-fluid">
+      <div className="container-fluid text-center">
         <h1>Bienvenue sur Palc ! </h1>
-        <img href="%PUBLIC_URL%/logopalccouleur.png" class="img-fluid" alt="Responsive image" />
+        <img src={logopalccouleur} class="img-fluid" alt="Responsive image" />
         <p> Mais qu'est ce que palc peut bien vouloir dire ?  Ca veut dire "Pinte à la clé"</p>
       </div>
     )
