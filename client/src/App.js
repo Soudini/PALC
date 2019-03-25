16b192f1d6691e92b02b42dd5ea15373e92ab57b
Raiki Ali <ali.raiki@student-cs.fr>
2019-03-26 00:27:51 +0100

    Fixing code

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,6 @@ import Cookies from 'universal-cookie';
 import $ from 'jquery';
 window.$ = $;
 
-/* je sais pas où mettre le code si dessous, donc  je le mets là (PS: c'est pour la searchBar)*/
-$("#inpt_search").on('focus', function () {
-  $(this).parent('label').addClass('active');
-});
-
-$("#inpt_search").on('blur', function () {
-  if ($(this).val().length == 0)
-    $(this).parent('label').removeClass('active');
-});
-/* ---------------------------------------------------------------------------------------------*/
-
 const cookies = new Cookies();
 
 const date = new Date();
