233ff29d7343b14a73f937f2b4339a8fcda5d20c
Raiki Ali <ali.raiki@student-cs.fr>
2019-03-26 00:26:42 +0100

    Fixing code

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,19 @@ import './App.css';
 import Main from './components/main.js';
 import Banner from './components/banner.js';
 import Cookies from 'universal-cookie';
+import $ from 'jquery';
+window.$ = $;
+
+/* je sais pas où mettre le code si dessous, donc  je le mets là (PS: c'est pour la searchBar)*/
+$("#inpt_search").on('focus', function () {
+  $(this).parent('label').addClass('active');
+});
+
+$("#inpt_search").on('blur', function () {
+  if ($(this).val().length == 0)
+    $(this).parent('label').removeClass('active');
+});
+/* ---------------------------------------------------------------------------------------------*/
 
 const cookies = new Cookies();
 
@@ -16,11 +29,11 @@ class App extends Component {
   render() {
 
     return (
-        <div className="container-fluid">
-          <Banner/>
-          <br></br><br></br><br></br><br></br>
-          <Main />
-        </div>
+      <div className="container-fluid">
+        <Banner />
+        <br></br><br></br><br></br><br></br>
+        <Main />
+      </div>
     );
   }
 }
