75f2d4a9fd24e2d46a0b04f814b2eb1bfc99819f
Raiki Ali <ali.raiki@student-cs.fr>
2019-03-27 00:04:49 +0100

    Fixing code

--- client/src/components/ad.js
+++ client/src/components/ad.js
@@ -45,6 +45,14 @@ export class Page extends Component { // full page view of the ad with the id wr
     let buttonDelete = null;
     let buttonUpdate = null;
 
+    if (this.state.data.thumbnail === "") {
+      this.state.data.thumbnail = image
+    }
+    if (this.state.data.thumbnail === null) {
+      this.state.data.thumbnail = image
+    }
+
+
     if (this.state.data && this.state.show_button) { // if we have some data and have to show the button
       buttonDelete = <button className="btn btn-danger col" onClick={this.handleDelete}>Supprimer l'annonce </button>;
       buttonUpdate = <button className="btn btn-primary col" style={{ "marginRight": "1rem", "marginLeft": "1rem" }} onClick={this.handleUpdate}>Modifier l'annonce</button>;
@@ -54,14 +62,6 @@ export class Page extends Component { // full page view of the ad with the id wr
     if (this.state.data) { //if we have data
 
       let carousel = null;
-
-      if (this.state.data.thumbnail === "") {
-        this.state.data.thumbnail = image
-      }
-      if (this.state.data.thumbnail === null) {
-        this.state.data.thumbnail = image
-      }
-
 
       if ((this.state.data.thumbnail != null & this.state.data.thumbnail != "") | this.state.data.image.length) { //if there is a thumbnail or images to show create the carousel
         carousel = <div id="carouselExampleControls" className="carousel slide col" data-ride="carousel">
