befa280dee48be89abc29eb4c2f9eda3b83dc440
Raiki Ali <ali.raiki@student-cs.fr>
2019-03-27 02:49:03 +0100

    Change other buttons

--- client/src/components/ad.js
+++ client/src/components/ad.js
@@ -50,7 +50,7 @@ export class Page extends Component { // full page view of the ad with the id wr
 
     if (this.state.data && this.state.show_button) { // if we have some data and have to show the button
       buttonDelete = <button className="btn btn-danger col" onClick={this.handleDelete} style={{ "border-radius": "50px", "padding": "10px" }}><i class="fa fa-trash" style={{ "color": "white" }}></i>Supprimer</button>;
-      buttonUpdate = <button className="btn btn-primary col" style={{ "marginRight": "1rem", "marginLeft": "1rem" }} onClick={this.handleUpdate}><i class='far fa-edit' style={{ "color": "white" }}></i>Modifier</button>;
+      buttonUpdate = <button className="btn btn-primary col" style={{ "marginRight": "1rem", "marginLeft": "1rem", "border-radius": "50px" }} onClick={this.handleUpdate}><i class='far fa-edit' style={{ "color": "white" }}></i>Modifier</button>;
 
     }
 
@@ -100,7 +100,7 @@ export class Page extends Component { // full page view of the ad with the id wr
                 <h5 className="text-center">{"Cette annonce a été créée par " + this.state.data.author}</h5>
                 <p className="card-text text-center" id="description">{this.state.data.description}</p>
                 <div className="row">
-                  <a className="col text-center" href={"https://linkcs.fr/user/" + this.state.data.author_login}><button className="btn btn-secondary col"  ><img src={logolinkcs} class="grey-img img-fluid" alt="Responsive image" /> LinkCS </button></a>
+                  <a className="col text-center" href={"https://linkcs.fr/user/" + this.state.data.author_login}><button className="btn btn-secondary col"  >LinkCS </button></a>
                   {buttonUpdate}
                   {buttonDelete}
                 </div>
