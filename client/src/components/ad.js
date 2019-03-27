b4acbd8f1cf1998f1a8124f950bf23378b099795
Raiki Ali <ali.raiki@student-cs.fr>
2019-03-28 00:18:09 +0100

    Change space between title and carousel

--- client/src/components/ad.js
+++ client/src/components/ad.js
@@ -90,7 +90,7 @@ export class Page extends Component { // full page view of the ad with the id wr
       return (
         <div className="col-lg-8">
           <div className="jumbotron fluid">
-            <div >
+            <div>
               <h1 className="text-center" id="titre_cancer">{this.state.data.title}</h1>
             </div>
             <div className="row align-items-center">
