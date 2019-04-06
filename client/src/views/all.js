c3984a6b31565b8b8436b656e4d5de1f659d2502
Raiki Ali <ali.raiki@student-cs.fr>
2019-04-06 16:37:20 +0200

    Revert to last commit

--- client/src/views/all.js
+++ client/src/views/all.js
@@ -1,24 +1,10 @@
 import React, { Component } from 'react';
 import axios from "axios";
 import Display from "../components/display.js";
-import logopalccouleur from "../files/logopalccouleur.png";
 export default class All extends Component {
-  render() {
-    return (
-      <div id="root">
-        <div class="app-loading">
-          <img id="icon_spinner" src={logopalccouleur} />
-          <svg class="spinner" viewBox="25 25 50 50">
-            <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" />
-          </svg>
-        </div>
-      </div>
-    )
-  }
 
   render() {
     return (
-
       <Display search={{}} />)
   }
 }
