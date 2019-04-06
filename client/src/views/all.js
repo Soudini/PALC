8f0e0d7e83bd272f7256c1f66883f707edf3087d
Raiki Ali <ali.raiki@student-cs.fr>
2019-04-06 16:07:47 +0200

    Loading screen in /all

--- client/src/views/all.js
+++ client/src/views/all.js
@@ -2,9 +2,22 @@ import React, { Component } from 'react';
 import axios from "axios";
 import Display from "../components/display.js";
 export default class All extends Component {
-
+  render(){
+    return(
+      <div id="root">
+    <div class="app-loading">
+      <img id="icon_spinner" src="./logopalc.png"/>
+      <svg class="spinner" viewBox="25 25 50 50">
+        <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" />
+      </svg>
+    </div>
+  </div>
+    )
+  }
+  
   render () {
       return (
+        
         <Display search = {{}}/>)
    }
 }
