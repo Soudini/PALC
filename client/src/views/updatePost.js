33ea0dc4e676b6cc883fa29459cb10a10b73b9ae
2018louysa <arnaud.louys@student-cs.fr>
2019-04-07 19:51:16 +0200

    fix

--- client/src/views/updatePost.js
+++ client/src/views/updatePost.js
@@ -60,7 +60,7 @@ class UpdatePost extends Component {
     else {
       return (
         <div className="app-loading">
-          <img id="icon_spinner" src="../../public/logopalc.png"/>
+          <img id="icon_spinner" src="./logopalc.png"/>
           <svg className="spinner" viewBox="25 25 50 50">
             <circle className="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" />
           </svg>
