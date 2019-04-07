7db2a7573af32e138e1fa86b986ed3ddc5960f5e
Raiki Ali <ali.raiki@student-cs.fr>
2019-04-08 00:14:25 +0200

    Erreur 404 design

--- client/src/views/page404.js
+++ client/src/views/page404.js
@@ -47,14 +47,21 @@ $(function () {
 })
 
 
-export default class Home extends Component {
+export default class Page404 extends Component {
+    handlePageChange = (page) => { // navigate between pages
+
+        this.props.history.push("/" + page);
+        if (document.getElementById("navbar").classList.contains("show")) {
+            document.getElementById("navbar-toggler").click();
+        }
+    }
+
     render() {
         return (
             <div>
                 <main>
                     <p>Erreur Page 404</p>
                     <br />
-                    <hr class="light my-4" />
                     <br />
                     <div class="chope">
                         <div class="mousse">
@@ -65,8 +72,22 @@ export default class Home extends Component {
                     </div>
                     <div class="anse">
                     </div>
+                    <br />
 
                 </main>
+                <div class="text-center">
+                    <div class="row">
+                        <div class="col">
+                            <a class="btn1 btn-primary1 btn-xl" onClick={() => this.handlePageChange("")}>&nbsp;
+                              Accueil&nbsp;
+              </a>
+                        </div>
+                        <div class="col">
+                            <a class="btn1 btn-primary1 btn-xl"
+                                onClick={() => this.handlePageChange("all")}>Annonces</a>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
