de12bbe8e506197c869735d5005151434f7811d7
Raiki Ali <ali.raiki@student-cs.fr>
2019-04-07 23:24:53 +0200

    Erreur 404 design

--- client/src/views/page404.js
+++ client/src/views/page404.js
@@ -1,58 +1,66 @@
 import React, { Component } from 'react';
 import axios from "axios";
 
+
+function loading(time) {
+    setTimeout(function () {
+        $('.biere').addClass('active');
+        setTimeout(function () {
+            $('.mousse > div').each(function () {
+                var scaleValue = Math.floor((Math.random() * 5) + 3);
+                $(this).css('opacity', 1);
+                $(this).css({
+                    '-webkit-transform': 'scale(' + scaleValue + ')',
+                    '-moz-transform': 'scale(' + scaleValue + ')',
+                    '-ms-transform': 'scale(' + scaleValue + ')',
+                    '-o-transform': 'scale(' + scaleValue + ')',
+                    'transform': 'scale(' + scaleValue + ')'
+                });
+            });
+            setTimeout(function () {
+                $('.mousse > div').each(function () {
+                    var scaleValue = 0;
+                    $(this).css('opacity', 1);
+                    $(this).css({
+                        '-webkit-transform': 'scale(' + scaleValue + ')',
+                        '-moz-transform': 'scale(' + scaleValue + ')',
+                        '-ms-transform': 'scale(' + scaleValue + ')',
+                        '-o-transform': 'scale(' + scaleValue + ')',
+                        'transform': 'scale(' + scaleValue + ')'
+                    });
+                });
+                setTimeout(function () {
+                    $('.biere').removeClass('active');
+                    setTimeout(function () {
+                        loading(time);
+                    }, time * 2);
+                }, time);
+            }, time * 2);
+        }, time);
+    }, time / 2);
+}
+$(function () {
+    loading(500);
+})
+
+
 export default class Home extends Component {
     render() {
         return (
-            <div id="beer1">
-                <div class="written" contenteditable>
-                    Erreur Page 404
-		        </div>
-            </div>
-            <div id="beer2" >
-                <div class="cup">
-                    <div></div>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                    <i class="bolha"></i>
-                </div>
+            <div>
+                <a target="_blank" style="display: block; position: fixed; z-index:9999; bottom: 10px; right: 10px; width: 60px;" href="http://www.yoannbraie.fr/board/"><img style="width: 100%;" src="http://images.yoannbraie.fr/logoyb_white.png" alt="logo" /></a>
+                <main>
+                    <div class="chope">
+                        <div class="mousse">
+                            <div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+                        </div>
+                        <div class="biere">
+                        </div>
+                    </div>
+                    <div class="anse">
+                    </div>
+                    <p>Erreur Page 404</p>
+                </main>
             </div>
         )
     }
