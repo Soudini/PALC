57181ad05d9f1fd7e18aa2624867d1ce8d153953
2018louysa <arnaud.louys@student-cs.fr>
2019-04-07 19:18:23 +0200

    test

--- client/src/views/updatePost.js
+++ client/src/views/updatePost.js
@@ -44,7 +44,7 @@ class UpdatePost extends Component {
 
   searchDataFromDb = () => {
     axios.post("/api/searchById", {id :this.props.match.params.id, auth : cookies.get("auth")})
-      .then(data => data.data).then(res => {for (let i in res.data) {this.setState({[i] : res.data[i]})}}).then(this.setState({loaded:true}));
+      .then(data => data.data).then(res => this.setState(res.data)).then(this.setState({loaded:true}));
     
   };
 
