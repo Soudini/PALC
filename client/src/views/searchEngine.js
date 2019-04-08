2c2b41b36897b245e5eaf1ed98492260a1f85ad7
Raiki Ali <ali.raiki@student-cs.fr>
2019-04-08 20:25:18 +0200

    Trying something

--- client/src/views/searchEngine.js
+++ client/src/views/searchEngine.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from "axios";
 import Display from "../components/display.js";
 import Cookies from 'universal-cookie';
+import AuncunResultat from "../files/AucunResultat.png";
 
 let cookies = new Cookies();
 
@@ -11,9 +12,26 @@ export default class Perso extends Component {
 
 
 
-  render () {
+  render() {
+    if (this.props.match.params.searchText == "") {
       return (
-        <Display search={{ "$or":[{"type":{"$regex": this.props.match.params.searchText, "$options" : "i"}},{"author":{"$regex": this.props.match.params.searchText, "$options" : "i"}},{"reward":{"$regex": this.props.match.params.searchText, "$options" : "i"}},{"description":{"$regex": this.props.match.params.searchText, "$options" : "i"}},{"title":{"$regex": this.props.match.params.searchText, "$options" : "i"}} ]}}/>
+        <section>
+          <div className="container-fondus ">
+            <div className="row">
+              <div className="col-lg-6 mx-auto text-center">
+
+                <br></br>
+                <img src={AuncunResultat} id="noresults_image" class="img-fluid" alt="Responsive image" />
+              </div>
+            </div>
+          </div>
+        </section>
       )
-   }
+    }
+    else {
+      return (
+        <Display search={{ "$or": [{ "type": { "$regex": this.props.match.params.searchText, "$options": "i" } }, { "author": { "$regex": this.props.match.params.searchText, "$options": "i" } }, { "reward": { "$regex": this.props.match.params.searchText, "$options": "i" } }, { "description": { "$regex": this.props.match.params.searchText, "$options": "i" } }, { "title": { "$regex": this.props.match.params.searchText, "$options": "i" } }] }} />
+      )
+    }
+  }
 }
