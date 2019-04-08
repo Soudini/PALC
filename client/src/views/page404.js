88b260625ba30a4e0b29d74caff2fe5cd13fa702
Raiki Ali <ali.raiki@student-cs.fr>
2019-04-08 18:07:44 +0200

    Erreur 404 design

--- client/src/views/page404.js
+++ client/src/views/page404.js
@@ -60,40 +60,22 @@ export default class Page404 extends Component {
         return (
             <div id="fond" class="text-center">
 
-                <main id="text-center">
-                    <p id="texte_erreur" class="text-center">Erreur <br></br> Page 404</p>
-                    <br />
-                    <br />
+                <div id="glass">
+                    <div id="beer"></div>
+                </div>
+                <div id="poignet"></div>
+                <div id="mousse_1"></div>
+                <div id="mousse_2"></div>
+                <div id="mousse_3"></div>
+                <div id="mousse_4"></div>
+                <div id="mousse_5"></div>
+                <div id="mousse_volante"></div>
+                <div id="mousse_interieur"></div>
+                <div id="mousse_interieur_2"></div>
+                <div id="mousse_interieur_3"></div>
+                <div id="mousse_interieur_4"></div>
 
-                    <br />
-                    <br />
-                    <div class="chope">
-                        <div class="mousse">
-                            <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
-                        </div>
-                        <div class="biere">
-                        </div>
-                    </div>
-                    <div class="anse">
-                    </div>
-                    <br />
-                    <div class="col-lg-8 mx-auto" id="boutons">
-                        <div class="text-center">
-                            <div class="row">
-                                <div class="col">
-                                    <a class="btn1 btn-primary1 btn-xl" onClick={() => this.handlePageChange("")}>&nbsp;
-                                      Accueil&nbsp;
-              </a>
-                                </div>
-                                <div class="col">
-                                    <a class="btn1 btn-primary1 btn-xl"
-                                        onClick={() => this.handlePageChange("all")}>Annonces</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </main>
-
+                <p id="beer_text">Erreur<br />Page 404</p>
             </div>
 
         )
