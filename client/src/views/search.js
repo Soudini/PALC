71f43ff4168c345d3ecfde7fb9ce9aa74bcf48d2
Raiki Ali <ali.raiki@student-cs.fr>
2019-04-08 22:05:58 +0200

    Trying something

--- client/src/views/search.js
+++ client/src/views/search.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 import axios from "axios";
 import Display from "../components/display.js";
+import AuncunResultat from "../files/AucunResultat.png";
+
 export default class Home extends Component {
 
 
-  render () {
+  render() {
+    if (this.props.match.params.searchText === "") {
       return (
-        <Display search = {{type: "search"}}/>
+        <section>
+          <div className="container-fondus ">
+            <div className="row">
+              <div className="col-lg-6 mx-auto text-center">
+
+                <br></br>
+                <img src={AuncunResultat} id="noresults_image" class="img-fluid" alt="Responsive image" />
+              </div>
+            </div>
+          </div>
+        </section>
       )
-   }
+    }
+    else {
+      return (
+        <Display search={{ type: "search" }} />
+      )
+    }
+  }
 }
