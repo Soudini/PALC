cf1d7efb32e42db36d271ee420811dbbe7032f92
Raiki Ali <ali.raiki@student-cs.fr>
2019-04-11 21:40:31 +0200

    Quick fixes

--- client/src/views/home.js
+++ client/src/views/home.js
@@ -17,7 +17,8 @@ export default class All extends Component {
               <h1 class="section-heading"> Bienvenue sur PALC ! </h1>
               <hr class="light my-4" />
               <img src={logopalccouleur} id="image_welcome_page" class="img-fluid" alt="Responsive image" />
-              <p> Mais qu'est ce que PALC peut bien vouloir dire ?  Ca veut dire "Pinte à la clé" ! Tu peux trouver ici les objets trouvés, ou faire une annonce si tu as trouvé un objet.</p>
+              <p> Mais qu'est ce que PALC peut bien vouloir dire ?  Ca veut dire "Pinte à la clé" ! Tu peux trouver ici les objets trouvés, ou faire une annonce si tu as trouvé un objet.<br />
+                Ce site est en période de test, si vous rencontrez un problème ou avez une remarque, n'hésitez pas à envoyer à mail à <a id="mail_contact" href="mailto:palc@ml.viarezo.fr">palc@ml.viarezo.fr</a></p>
             </div>
           </div>
         </div>
