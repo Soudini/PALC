378c3fc33f4449d94c34fadf00fc88186b5c5ccf
Raiki Ali <ali.raiki@student-cs.fr>
2019-04-12 18:23:01 +0200

    Correcting spelling mistake in home

--- client/src/views/home.js
+++ client/src/views/home.js
@@ -18,7 +18,7 @@ export default class All extends Component {
               <hr class="light my-4" />
               <img src={logopalccouleur} id="image_welcome_page" class="img-fluid" alt="Responsive image" />
               <p> Mais qu'est ce que PALC peut bien vouloir dire ?  Ca veut dire "Pinte à la clé" ! Tu peux trouver ici les objets trouvés, ou faire une annonce si tu as trouvé un objet.<br />
-                Ce site est en période de test, si vous rencontrez un problème ou avez une remarque, n'hésitez pas à envoyer à mail à <a id="mail_contact" href="mailto:palc@ml.viarezo.fr">palc@ml.viarezo.fr</a></p>
+                Ce site est en période de test, si vous rencontrez un problème ou avez une remarque, n'hésitez pas à envoyer un mail à <a id="mail_contact" href="mailto:palc@ml.viarezo.fr">palc@ml.viarezo.fr</a></p>
             </div>
           </div>
         </div>
