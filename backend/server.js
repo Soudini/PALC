d467278181cee6f306b5cb0f4dda8b4e8f7597f6
2018louysa <arnaud.louys@student-cs.fr>
2019-04-12 16:37:11 +0200

    add bureau to admin list

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const dbRoute = config.db_address;//"mongodb://localhost/ads"
 let keyEncrypt = secrets.keyEncrypt;
 const reCaptchaKey = config.reCaptchaKey ? config.reCaptchaKey : secrets.captcha.reCaptchaKey;
 console.log(reCaptchaKey);
-let admin = ["2018louysa","2018bekaddoul","2018raikia"];
+let admin = ["2018louysa","2018bekaddoul","2018raikia", "2018herlicqb", "2018lamboleyk", "2018houedep"];
 let ban = [];
 
 
